c01dd021bf371e147dc7f72ed4f67234134a809b
git-anurag-hub <aguptaking@gmail.com>
2021-09-28 23:51:51 +0530

    Home Base

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import TextLoop from "react-text-loop";
+import Zoom from "react-reveal/Zoom";
 
 export default class Home extends Component {
   state = {
     x: 0,
     y: 0,
+    top: 0,
   };
+
+  componentDidMount() {
+    window.onscroll = () => {
+      this.setState({ top: document.documentElement.scrollTop });
+    };
+  }
 
   handleMouseMove = (e) => {
     this.setState({
@@ -15,9 +23,10 @@ export default class Home extends Component {
   };
 
   render() {
+    console.log(this.state.top);
     return (
-      <div>
-        <div className="font-pop">
+      <div className="font-pop">
+        <div>
           <div
             className="px-72 text-center text-5xl font-extrabold h-screen flex items-center text-gray-800 leading-relaxed"
             style={{
@@ -47,29 +56,42 @@ export default class Home extends Component {
               <div>the patient disease data.</div>
             </div>
           </div>
-          <div className="p-10">
-            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum
-            dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet,
-            consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur
-            adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum
-            dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet,
-            consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur
-            adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum
-            dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet,
-            consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur
-            adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum
-            dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet,
-            consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur
-            adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          </div>
+          <Zoom when={this.state.top > 210 ? true : false}>
+            <div className="p-10">
+              <div className="flex items-center px-10">
+                <div className="text-lg text-gray-700 leading-loose">
+                  The <span className="font-semibold">ELIXIR Cloud and AAI</span> project aims to
+                  leverage a coordinated network of{" "}
+                  <a
+                    href="https://elixir-europe.org/about-us/who-we-are/nodes"
+                    className="text-elixirblue font-semibold hover:underline"
+                  >
+                    ELIXIR Nodes
+                  </a>{" "}
+                  to deliver a{" "}
+                  <a
+                    href="https://www.ga4gh.org/"
+                    className="text-elixirblue font-semibold hover:underline"
+                  >
+                    GA4GH
+                  </a>{" "}
+                  standards-compliant federated environment to enable population scale genomic and
+                  phenotypic data analysis across international boundaries and a potential
+                  infrastructure to enable{" "}
+                  <a
+                    href="https://ec.europa.eu/digital-single-market/en/news/towards-access-least-1-million-genomes-eu-2022-1-year"
+                    className="text-elixirblue font-semibold hover:underline"
+                  >
+                    1M Genome analysis
+                  </a>
+                  .
+                </div>
+                <div>
+                  <img src="/Earth.svg" alt="globe"></img>
+                </div>
+              </div>
+            </div>
+          </Zoom>
         </div>
       </div>
     );
