9dbc119efd4cd1eb5dabae545848120c264e3b51
Tanya Kumari <95133237+tanya17-05@users.noreply.github.com>
2022-12-31 00:21:12 +0530

    fix: add missing space in text loop (#58)

--- components/Home.js
+++ components/Home.js
@@ -5,101 +5,102 @@ import Zoom from "react-reveal/Zoom";
 import window from "global/window";
 
 function Home() {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-  const [innerWidth, setinnerWidth] = useState(1000);
-  const backgroundImgUrl = "url(/Landing_Dark.svg)";
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+    const [innerWidth, setinnerWidth] = useState(1000);
+    const backgroundImgUrl = "url(/Landing_Dark.svg)";
 
-  const handleMouseMove = (e) => {
-    setX(e.clientX);
-    setY(e.clientY);
-    setinnerWidth(window.innerWidth);
-  };
+    const handleMouseMove = (e) => {
+        setX(e.clientX);
+        setY(e.clientY);
+        setinnerWidth(window.innerWidth);
+    };
 
-  const renderTextLoop = () => (
-    <span className="block md:inline">
-      <TextLoop springConfig={{ stiffness: 180, damping: 8 }} noWrap={false}>
-        <span className="text-elixirred">Findable</span>
-        <span className="text-elixirgreen">Accessible</span>
-        <span className="text-elixiryellow">Interoperable</span>
-        <span className="text-elixirblue">Reusable</span>
-        <div>
-          <span className="text-elixirred">F</span>
-          <span className="text-elixirgreen">A</span>
-          <span className="text-elixiryellow">I</span>
-          <span className="text-elixirblue">R</span>
-        </div>
-      </TextLoop>
-    </span>
-  );
+    const renderTextLoop = () => (
+        <span className="block md:inline">
+            <TextLoop springConfig={{ stiffness: 180, damping: 8 }} noWrap={false}>
+                <span className="text-elixirred">Findable</span>
+                <span className="text-elixirgreen">Accessible</span>
+                <span className="text-elixiryellow">Interoperable</span>
+                <span className="text-elixirblue">Reusable</span>
+                <div>
+                    <span className="text-elixirred">F</span>
+                    <span className="text-elixirgreen">A</span>
+                    <span className="text-elixiryellow">I</span>
+                    <span className="text-elixirblue">R</span>
+                </div>
+            </TextLoop>
+        </span>
+    );
 
-  return (
-    <div className="font-pop dark:bg-gray-800 dark:text-white">
-      <div>
-        <div
-          className="px-10 text-center text-4xl md:text-5xl font-extrabold h-screen flex items-center justify-center text-gray-800"
-          style={{
-            backgroundImage: innerWidth >= 650 ? backgroundImgUrl : "",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "101% 101%",
-            backgroundPositionX: `${(x - 1000) / 100}px`,
-            backgroundPositionY: `${y / 50}px`,
-          }}
-          onMouseMove={(e) => {
-            handleMouseMove(e);
-          }}
-        >
-          <Zoom>
-            <div className="leading-relaxed dark:text-gray-200">
-              {/* <div className="text-xl md:text-3xl mb-1 leading-relaxed font-bold md:font-extrabold">
+    return (
+        <div className="font-pop dark:bg-gray-800 dark:text-white">
+            <div>
+                <div
+                    className="px-10 text-center text-4xl md:text-5xl font-extrabold h-screen flex items-center justify-center text-gray-800"
+                    style={{
+                        backgroundImage: innerWidth >= 650 ? backgroundImgUrl : "",
+                        backgroundRepeat: "no-repeat",
+                        backgroundSize: "101% 101%",
+                        backgroundPositionX: `${(x - 1000) / 100}px`,
+                        backgroundPositionY: `${y / 50}px`,
+                    }}
+                    onMouseMove={(e) => {
+                        handleMouseMove(e);
+                    }}
+                >
+                    <Zoom>
+                        <div className="leading-relaxed dark:text-gray-200">
+                            {/* <div className="text-xl md:text-3xl mb-1 leading-relaxed font-bold md:font-extrabold">
                 ELIXIR Cloud &#38; AAI
               </div> */}
-              Making Cloud Infrastructure for{" "}
-              <span className="inline md:block">
-                the Life Sciences {renderTextLoop()}
-              </span>
-            </div>
-          </Zoom>
-        </div>
-        <Zoom>
-          <div className="p-0 md:px-10">
-            <div className="flex items-center px-10 flex-col md:flex-row">
-              <div className="text-lg text-gray-700 dark:text-gray-200 leading-loose text-justify">
-                The{" "}
-                <span className="font-semibold">
-                  ELIXIR Cloud and Authentication and Authorization
-                  Infrastructure (AAI)
-                </span>{" "}
-                project aims to leverage a coordinated network of{" "}
-                <a
-                  href="https://elixir-europe.org/about-us/who-we-are/nodes"
-                  className="text-elixirblue font-semibold hover:underline"
-                >
-                  ELIXIR Nodes
-                </a>{" "}
-                to deliver a{" "}
-                <a
-                  href="https://www.ga4gh.org/"
-                  className="text-elixirblue font-semibold hover:underline"
-                >
-                  GA4GH
-                </a>{" "}
-                standards-compliant federated environment to enable population
-                scale genomic and phenotypic data analysis across international
-                boundaries.{" "}
-                <Link href="/overview" passHref>
-                  <span className="text-elixirblue font-semibold hover:underline cursor-pointer">
-                    More...
-                  </span>
-                </Link>
-              </div>
-              <div>
-                <img src="/Earth.svg" alt="globe" width="auto" height="auto" />
-              </div>
-            </div>
-          </div>
-        </Zoom>
-        {/* <Zoom>
+                            Making Cloud Infrastructure for{" "}
+                            <span className="inline md:block">
+                                the Life Sciences{" "}
+                                {renderTextLoop()}
+                            </span>
+                        </div>
+                    </Zoom>
+                </div>
+                <Zoom>
+                    <div className="p-0 md:px-10">
+                        <div className="flex items-center px-10 flex-col md:flex-row">
+                            <div className="text-lg text-gray-700 dark:text-gray-200 leading-loose text-justify">
+                                The{" "}
+                                <span className="font-semibold">
+                                    ELIXIR Cloud and Authentication and Authorization
+                                    Infrastructure (AAI)
+                                </span>{" "}
+                                project aims to leverage a coordinated network of{" "}
+                                <a
+                                    href="https://elixir-europe.org/about-us/who-we-are/nodes"
+                                    className="text-elixirblue font-semibold hover:underline"
+                                >
+                                    ELIXIR Nodes
+                                </a>{" "}
+                                to deliver a{" "}
+                                <a
+                                    href="https://www.ga4gh.org/"
+                                    className="text-elixirblue font-semibold hover:underline"
+                                >
+                                    GA4GH
+                                </a>{" "}
+                                standards-compliant federated environment to enable population
+                                scale genomic and phenotypic data analysis across international
+                                boundaries.{" "}
+                                <Link href="/overview" passHref>
+                                    <span className="text-elixirblue font-semibold hover:underline cursor-pointer">
+                                        More...
+                                    </span>
+                                </Link>
+                            </div>
+                            <div>
+                                <img src="/Earth.svg" alt="globe" width="auto" height="auto" />
+                            </div>
+                        </div>
+                    </div>
+                </Zoom>
+                {/* <Zoom>
           <div className="p-0 md:px-10">
             <div className="flex items-center px-10 flex-col md:flex-row">
               <div className="text-lg text-gray-700 leading-loose">
@@ -114,9 +115,9 @@ function Home() {
             </div>
           </div>
         </Zoom> */}
-      </div>
-    </div>
-  );
+            </div>
+        </div>
+    );
 }
 
 export default Home;
