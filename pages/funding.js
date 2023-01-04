4c43adaa97373cd2f4dee574c80919217b09112f
tanya17-05 <tanyakumari332@gmail.com>
2023-01-04 19:05:34 +0530

    add: funders page

--- pages/funding.js
+++ pages/funding.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { NextSeo } from "next-seo";
+import Funders from "../components/Funders";
+import getFunders from "../utils/funder";
 
-function Funding() {
-  return <div />;
+function Funding({ funders }) {
+  return (
+    <>
+      <NextSeo
+        title="Partners"
+        description="ELIXIR Cloud & AAI collobrative funders."
+      />
+      <Funders funders={funders} />
+    </>
+  );
 }
+
+export const getStaticProps = async () => {
+  const data = await getFunders();
+
+  return {
+    props: {
+      funders: data,
+    },
+    revalidate: 30,
+  };
+};
 
 export default Funding;
