78eb73d13ffa48b78cf1f5da328aa1f9d5a9a90a
samyak-max <samyak_c@bt.iitr.ac.in>
2023-01-17 22:19:32 +0530

    remove targeting and analytics cookies, remove the onChange, onAccept, onFirstAction functions

--- components/CookieConsent.js
+++ components/CookieConsent.js
@@ -1,14 +1,10 @@
-import { useEffect } from "react";
-import pluginConfig from "./CookieConsentConfig";
-import "vanilla-cookieconsent";
+import { useEffect } from 'react';
+import pluginConfig from './CookieConsentConfig';
+import 'vanilla-cookieconsent';
 
 const CookieConsent = () => {
   useEffect(() => {
-    /**
-     * useEffect is executed twice (React 18+),
-     * make sure the plugin is initialized and executed once
-     */
-    if (!document.getElementById("cc--main")) {
+    if (!document.getElementById('cc--main')) {
       window.CookieConsentApi = window.initCookieConsent();
       window.CookieConsentApi.run(pluginConfig);
     }
--- components/CookieConsentConfig.js
+++ components/CookieConsentConfig.js
@@ -2,127 +2,66 @@
  * @type {UserConfig}
  */
 const pluginConfig = {
-  current_lang: "en",
-  autoclear_cookies: true, // default: false
-  page_scripts: true, // default: false
-  test: "ciao",
-  force_consent: true, // default: false
-  // mode: 'opt-in'                          // default: 'opt-in'; value: 'opt-in' or 'opt-out'
-  // delay: 0,                               // default: 0
-  // auto_language: null                     // default: null; could also be 'browser' or 'document'
-  // autorun: true,                          // default: true
-  // hide_from_bots: false,                  // default: false
-  // remove_cookie_tables: false             // default: false
-  // cookie_name: 'cc_cookie',               // default: 'cc_cookie'
-  // cookie_expiration: 182,                 // default: 182 (days)
-  // cookie_necessary_only_expiration: 182   // default: disabled
-  // cookie_domain: location.hostname,       // default: current domain
-  // cookie_path: '/',                       // default: root
-  // cookie_same_site: 'Lax',                // default: 'Lax'
-  // use_rfc_cookie: false,                  // default: false
-  // revision: 0,                            // default: 0
-
-  onFirstAction: function (user_preferences, cookie) {
-    // callback triggered only once
-  },
-
-  onAccept: function (cookie) {
-    // ...
-  },
-
-  onChange: function (cookie, changed_preferences) {
-    // ...
-  },
+  current_lang: 'en',
+  autoclear_cookies: true,
+  page_scripts: true,
+  test: 'ciao',
+  force_consent: true,
   gui_options: {
     consent_modal: {
-      layout: "bar", // box/cloud/bar
-      position: "bottom center", // bottom/middle/top + left/right/center
-      transition: "slide", // zoom/slide
-      swap_buttons: false, // enable to invert buttons
+      layout: 'bar',
+      position: 'bottom center',
+      transition: 'slide',
+      swap_buttons: false,
     },
   },
   languages: {
     en: {
       consent_modal: {
-        title: "Cookie consent",
-        description: `We use cookies to recognize your repeated visits and preferences, as well as to measure the effectiveness of our documentation and whether users find what they're searching for. With your consent, you're helping us to make our documentation better. <button type="button" data-cc="c-settings" class="cc-link"> Let me choose</button>`,
+        title: 'Cookie consent',
+        description:
+          'We use cookies to recognize your repeated visits and preferences, as well as to measure the effectiveness of our documentation and whether users find what they\'re searching for. With your consent, you\'re helping us to make our documentation better. <button type="button" data-cc="c-settings" class="cc-link"> Let me choose</button>',
         primary_btn: {
-          text: "Accept all",
-          role: "accept_all", // 'accept_selected' or 'accept_all'
+          text: 'Accept all',
+          role: 'accept_all',
         },
         secondary_btn: {
-          text: "Reject all",
-          role: "accept_necessary", // 'settings' or 'accept_necessary'
+          text: 'Reject all',
+          role: 'accept_necessary',
         },
       },
       settings_modal: {
-        title: "Cookie Settings",
-        save_settings_btn: "Save settings",
-        accept_all_btn: "Accept all",
-        reject_all_btn: "Reject all",
-        close_btn_label: "Close",
+        title: 'Cookie Settings',
+        save_settings_btn: 'Save settings',
+        accept_all_btn: 'Accept all',
+        reject_all_btn: 'Reject all',
+        close_btn_label: 'Close',
         cookie_table_headers: [
-          { col1: "Name" },
-          { col2: "Domain" },
-          { col3: "Expiration" },
-          { col4: "Description" },
+          { col1: 'Name' },
+          { col2: 'Domain' },
+          { col3: 'Expiration' },
+          { col4: 'Description' },
         ],
         blocks: [
           {
-            title: "Cookie usage 📢",
+            title: 'Cookie usage 📢',
             description:
-              'We use cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details relative to cookies and other sensitive data, please read the full <a href="#" class="cc-link">privacy policy</a>.',
+              'We use cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details relative to cookies and other sensitive data, please read the full <a href="/policy" class="cc-link">privacy policy</a>.',
           },
           {
-            title: "Strictly necessary cookies",
+            title: 'Strictly necessary cookies',
             description:
-              "These cookies are essential for the proper functioning of the website. Without these cookies, the website would not work properly",
+              'These cookies are essential for the proper functioning of the website. Without these cookies, the website would not work properly',
             toggle: {
-              value: "necessary",
+              value: 'necessary',
               enabled: true,
-              readonly: true, // cookie categories with readonly=true are all treated as "necessary cookies"
+              readonly: true,
             },
           },
           {
-            title: "Performance and Analytics cookies",
+            title: 'More information',
             description:
-              "These cookies allow the website to remember the choices you have made in the past",
-            toggle: {
-              value: "analytics", // your cookie category
-              enabled: false,
-              readonly: false,
-            },
-            cookie_table: [
-              // list of all expected cookies
-              {
-                col1: "^_ga", // match all cookies starting with "_ga"
-                col2: "google.com",
-                col3: "2 years",
-                col4: "description ...",
-                is_regex: true,
-              },
-              {
-                col1: "_gid",
-                col2: "google.com",
-                col3: "1 day",
-                col4: "description ...",
-              },
-            ],
-          },
-          {
-            title: "Advertisement and Targeting cookies",
-            description:
-              "These cookies collect information about how you use the website, which pages you visited and which links you clicked on. All of the data is anonymized and cannot be used to identify you",
-            toggle: {
-              value: "targeting",
-              enabled: false,
-              readonly: false,
-            },
-          },
-          {
-            title: "More information",
-            description:
-              'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="#yourcontactpage">contact us</a>.',
+              'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="mailto:cloud-service@elixir-europe.org">contact us</a>.',
           },
         ],
       },
