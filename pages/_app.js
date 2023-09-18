b92742260639122a75eb96483a0b9620edaed544
Anurag Gupta <me@anuragxd.com>
2023-09-18 12:28:28 +0530

    feat: GA cookies

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,50 @@
 import { useEffect } from 'react';
+import Script from 'next/script';
 import Layout from '../components/Layout';
 import '../styles/globals.css';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../next-seo.config';
 import 'vanilla-cookieconsent/dist/cookieconsent.css';
 import CookieConsent from '../components/CookieConsent';
+
+function GAScript() {
+  return (
+    <>
+      <Script id="google-analytics-consent">
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('consent', 'default', {
+        'ad_storage': 'denied',
+        'analytics_storage': 'denied'
+      });
+    `}
+      </Script>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
+      />
+      <Script id="google-analytics">
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', '${process.env.GA_MEASUREMENT_ID}');
+    `}
+      </Script>
+      <Script id="google-analytics-consent-granted">
+        {`
+      function GAConsentGranted() {
+        gtag('consent', 'update', {
+          'ad_storage': 'granted',
+          'analytics_storage': 'granted'
+        });
+      }
+    `}
+      </Script>
+    </>
+  );
+}
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
@@ -24,8 +64,10 @@ function MyApp({ Component, pageProps }) {
       });
     }
   }, []);
+
   return (
     <>
+      {process.env.NODE_ENV === 'production' && <GAScript />}
       <DefaultSeo {...SEO} />
       <Layout>
         <Component {...pageProps} />
